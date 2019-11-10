8a9c8b87373ed9fc8f0251387de6aa6fb76d6e87
Yuxuan Zhou <yuxuanz6@illinois.edu>
2019-11-10 17:03:54 -0600

    Add files via upload

---
+++ 2018spr_course/CS436 -- Computer network lab/LAB2/flow-tools-0.66/acconfig.h
@@ -0,0 +1,23 @@
+
+/* Name of package.  */
+#undef PACKAGE
+
+/* Version of package.  */
+#undef VERSION
+
+/* Linux */
+#undef _BSD_SOURCE
+
+/* Linux */
+#undef HAVE_FEATURES_H
+
+/* BSD socket */
+#undef HAVE_SOCK_SIN_LEN
+
+/* MYSQL */
+#undef HAVE_MYSQL
+
+/* DEC */
+#undef HAVE_LL_STRTOUL
+
+
